02f1e6958c91d6b3f141b0130468806c4c266839
Manuel Fuchs <manuel.fuchs@softaware.at>
2026-02-05 10:46:49 +0100

    Remove template

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,7 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  template: ` <router-outlet /> `,
   imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('SignalFormsDemo');
-}
+export class App {}
